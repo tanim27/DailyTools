21f456d69b0c44d93c9285f08ba19dcd7d7cd0ae
Tanim <abdullahtanim27@gmail.com>
2024-12-13 02:15:09 +0600

    height issue resolved

--- src/app/page.js
+++ src/app/page.js
@@ -4,30 +4,32 @@ import { Button } from '@mui/material'
 
 const page = () => {
 	return (
-		<div className='min-h-screen flex justify-center items-center bg-gray-200 w-full h-full'>
-			<div className='flex flex-col md:flex-row justify-between items-center space-y-5 md:space-y-0 h-full w-[90%] md:w-[30%] sm:w-[60%]'>
-				<div>
-					<Button
-						size='medium'
-						variant='contained'
-						color='primary'
-						href='todos'
-						startIcon={<FormatListBulletedRoundedIcon />}
-					>
-						To-Do
-					</Button>
-				</div>
+		<div className='min-h-screen'>
+			<div className='min-h-screen flex justify-center items-center bg-gray-200 w-full h-full'>
+				<div className='flex flex-col md:flex-row justify-between items-center space-y-5 md:space-y-0 h-full w-[90%] md:w-[30%] sm:w-[60%]'>
+					<div>
+						<Button
+							size='medium'
+							variant='contained'
+							color='primary'
+							href='todos'
+							startIcon={<FormatListBulletedRoundedIcon />}
+						>
+							To-Do
+						</Button>
+					</div>
 
-				<div>
-					<Button
-						size='medium'
-						variant='contained'
-						color='secondary'
-						href='notes'
-						startIcon={<NotesRoundedIcon />}
-					>
-						Notes
-					</Button>
+					<div>
+						<Button
+							size='medium'
+							variant='contained'
+							color='secondary'
+							href='notes'
+							startIcon={<NotesRoundedIcon />}
+						>
+							Notes
+						</Button>
+					</div>
 				</div>
 			</div>
 		</div>
