3ff1c27abc93aea884ce4724de5f51948d494373
Tanim <abdullahtanim27@gmail.com>
2024-12-13 01:30:39 +0600

    Daily tools app created successfully

---
+++ src/components/Todo.jsx
@@ -0,0 +1,189 @@
+'use client'
+
+import AddIcon from '@mui/icons-material/Add'
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline'
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded'
+import { Alert, Button, IconButton, Snackbar } from '@mui/material'
+
+import { useEffect, useState } from 'react'
+
+const Todo = () => {
+	const [task, setTask] = useState('')
+	const [tasks, setTasks] = useState([])
+	const [snackbar, setSnackbar] = useState({
+		open: false,
+		message: '',
+		severity: 'success',
+	})
+
+	// Load tasks from local storage on component mount
+	useEffect(() => {
+		const storedTasks = JSON.parse(localStorage.getItem('tasks'))
+		if (storedTasks) {
+			try {
+				setTasks(storedTasks)
+			} catch (error) {
+				console.error('Error parsing tasks from local storage:', error)
+				localStorage.removeItem('tasks')
+			}
+		}
+	}, [])
+
+	// Save tasks to local storage whenever they change
+	useEffect(() => {
+		if (tasks.length > 0) {
+			localStorage.setItem('tasks', JSON.stringify(tasks))
+		} else {
+			localStorage.removeItem('tasks')
+		}
+	}, [tasks])
+
+	const addTask = () => {
+		if (task.trim()) {
+			const newTasks = [...tasks, { title: task.trim(), completed: false }]
+			setTasks(newTasks)
+			setTask('')
+			showSnackBar('Task added successfully!', 'success')
+		} else {
+			showSnackBar('Task cannot be empty', 'error')
+		}
+	}
+
+	const deleteTask = (index) => {
+		const newTasks = [...tasks]
+		newTasks.splice(index, 1)
+		setTasks(newTasks)
+		showSnackBar(`Task ${index + 1} deleted`, 'info')
+	}
+
+	const toggleTaskCompletion = (index) => {
+		const newTasks = tasks.map((task, i) =>
+			i === index ? { ...task, completed: !task.completed } : task,
+		)
+		setTasks(newTasks)
+
+		const updatedTask = newTasks[index]
+		showSnackBar(
+			`Task ${index + 1} ${
+				updatedTask.completed ? 'completed' : 'marked as incomplete'
+			}`,
+			'success',
+		)
+	}
+
+	const handleDeleteAll = () => {
+		setTasks([])
+		showSnackBar('All tasks deleted', 'warning')
+	}
+
+	const showSnackBar = (message, severity) => {
+		setSnackbar({ open: true, message, severity })
+	}
+
+	const handleCloseSnackbar = () => {
+		setSnackbar({ ...snackbar, open: false })
+	}
+
+	return (
+		<div className='min-h-screen flex flex-col items-center bg-gray-200 py-10'>
+			<h1 className='text-center text-black text-5xl font-bold mb-5'>To Do</h1>
+
+			<div className='flex flex-col items-center space-y-2 mb-5 w-[90%] sm:w-[70%] md:w-[40%]'>
+				<input
+					type='text'
+					placeholder='Task name'
+					value={task}
+					onChange={(event) => setTask(event.target.value)}
+					className='text-black px-4 py-2 border rounded-lg shadow focus:outline-none focus:border-blue-500 w-full'
+				/>
+				<div className='flex justify-between w-full'>
+					<Button
+						size='small'
+						variant='contained'
+						color='success'
+						startIcon={<AddIcon />}
+						onClick={addTask}
+					>
+						Add
+					</Button>
+					<Button
+						size='small'
+						variant='contained'
+						color='error'
+						startIcon={<DeleteRoundedIcon />}
+						onClick={handleDeleteAll}
+					>
+						Delete All
+					</Button>
+				</div>
+			</div>
+
+			{tasks.length > 0 ? (
+				<div className='flex flex-col items-start w-[90%]'>
+					<h2 className='text-black text-3xl font-semibold '>To-do list:</h2>
+					<ul className='flex flex-col justify-center items-center w-full'>
+						{tasks.map((task, index) => (
+							<li
+								key={index}
+								className={`flex flex-col px-4 py-2 mb-2 rounded shadow w-full ${
+									task.completed
+										? 'bg-green-200 text-black line-through'
+										: 'bg-white text-black'
+								}`}
+							>
+								<div className='flex justify-between items-center w-full'>
+									<h4 className='text-2xl sm:text-xl font-semibold w-[80%]'>
+										{task.title.length > 20
+											? `${task.title.substring(0, 20)}...`
+											: task.title}
+									</h4>
+									<div className='flex justify-between space-x-2'>
+										<IconButton
+											aria-label='mark as done'
+											onClick={() => toggleTaskCompletion(index)}
+										>
+											<CheckCircleOutlineIcon
+												color={task.completed ? 'success' : 'action'}
+											/>
+										</IconButton>
+										<IconButton
+											aria-label='delete'
+											color='error'
+											onClick={() => deleteTask(index)}
+										>
+											<DeleteRoundedIcon />
+										</IconButton>
+									</div>
+								</div>
+							</li>
+						))}
+					</ul>
+				</div>
+			) : (
+				<div className='flex justify-center items-center py-20'>
+					<h2 className='font-bold text-3xl sm:text-4xl md:text-5xl text-gray-600'>
+						Empty!
+					</h2>
+				</div>
+			)}
+
+			{/* Snackbar */}
+			<Snackbar
+				open={snackbar.open}
+				autoHideDuration={3000}
+				onClose={handleCloseSnackbar}
+				anchorOrigin={{ vertical: 'bottom-left', horizontal: 'center' }}
+			>
+				<Alert
+					onClose={handleCloseSnackbar}
+					severity={snackbar.severity}
+					sx={{ width: '100%' }}
+				>
+					{snackbar.message}
+				</Alert>
+			</Snackbar>
+		</div>
+	)
+}
+
+export default Todo
