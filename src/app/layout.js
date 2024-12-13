ab021a4d3f46ba8d72bc40b020d107d4e87c0e63
Tanim <abdullahtanim27@gmail.com>
2024-12-13 17:59:51 +0600

    sd

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,4 @@
 import '@/styles/globals.css'
-import HomeRoundedIcon from '@mui/icons-material/HomeRounded'
-import { Fab } from '@mui/material'
 
 export const metadata = {
 	title: 'Daily Tools',
@@ -11,14 +9,14 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
 			<body className='relative'>
-				<div className='h-[100vh]'>{children}</div>
+				<div className=''>{children}</div>
 				{/* <div>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
 					laudantium maxime, omnis vero voluptatum in adipisci assumenda quis
 					doloremque mollitia ducimus, expedita blanditiis, ad fugit dicta
 					obcaecati nam exercitationem incidunt!
 				</div> */}
-				<div className='fixed bottom-6 right-6'>
+				{/* <div className='fixed bottom-6 right-6'>
 					<Fab
 						size='small'
 						color='warning'
@@ -27,7 +25,7 @@ export default function RootLayout({ children }) {
 					>
 						<HomeRoundedIcon />
 					</Fab>
-				</div>
+				</div> */}
 			</body>
 		</html>
 	)
--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,39 @@
 'use client'
 
-import FormatListBulletedRoundedIcon from '@mui/icons-material/FormatListBulletedRounded'
-import NotesRoundedIcon from '@mui/icons-material/NotesRounded'
-import { Button } from '@mui/material'
-
 const page = () => {
 	return (
-		<div className='flex justify-center items-center bg-gray-200 w-full h-[100vh]'>
-			<div className='flex flex-col md:flex-row justify-between items-center space-y-5 md:space-y-0 w-[90%] md:w-[30%] sm:w-[60%]'>
-				<div>
-					<Button
-						size='medium'
-						variant='contained'
-						color='primary'
-						href='todos'
-						startIcon={<FormatListBulletedRoundedIcon />}
-					>
-						To-Do
-					</Button>
-				</div>
+		// <div className='flex justify-center items-center bg-gray-200 w-full h-[100vh]'>
+		// 	<div className='flex flex-col md:flex-row justify-between items-center space-y-5 md:space-y-0 w-[90%] md:w-[30%] sm:w-[60%]'>
+		// 		<div>
+		// 			<Button
+		// 				size='medium'
+		// 				variant='contained'
+		// 				color='primary'
+		// 				href='todos'
+		// 				startIcon={<FormatListBulletedRoundedIcon />}
+		// 			>
+		// 				To-Do
+		// 			</Button>
+		// 		</div>
 
-				<div>
-					<Button
-						size='medium'
-						variant='contained'
-						color='secondary'
-						href='notes'
-						startIcon={<NotesRoundedIcon />}
-					>
-						Notes
-					</Button>
-				</div>
-			</div>
+		// 		<div>
+		// 			<Button
+		// 				size='medium'
+		// 				variant='contained'
+		// 				color='secondary'
+		// 				href='notes'
+		// 				startIcon={<NotesRoundedIcon />}
+		// 			>
+		// 				Notes
+		// 			</Button>
+		// 		</div>
+		// 	</div>
+		// </div>
+		<div>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+			exercitationem, ipsam libero voluptate, veritatis quod dolorem animi porro
+			necessitatibus rem aperiam iste nam, assumenda ea distinctio soluta ipsa
+			labore. Commodi!
 		</div>
 	)
 }
