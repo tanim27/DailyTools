b3019b8832b26eead5cd9f93a8502ffc532fe502
Tanim <abdullahtanim27@gmail.com>
2024-12-13 17:27:41 +0600

    resolved

--- src/app/page.js
+++ src/app/page.js
@@ -4,32 +4,30 @@ import { Button } from '@mui/material'
 
 const page = () => {
 	return (
-		<div className='min-h-screen'>
-			<div className='min-h-screen flex justify-center items-center bg-gray-200 w-full h-full'>
-				<div className='flex flex-col md:flex-row justify-between items-center space-y-5 md:space-y-0 h-full w-[90%] md:w-[30%] sm:w-[60%]'>
-					<div>
-						<Button
-							size='medium'
-							variant='contained'
-							color='primary'
-							href='todos'
-							startIcon={<FormatListBulletedRoundedIcon />}
-						>
-							To-Do
-						</Button>
-					</div>
+		<div className='flex justify-center items-center bg-gray-200 w-full h-[100vh]'>
+			<div className='flex flex-col md:flex-row justify-between items-center space-y-5 md:space-y-0 w-[90%] md:w-[30%] sm:w-[60%]'>
+				<div>
+					<Button
+						size='medium'
+						variant='contained'
+						color='primary'
+						href='todos'
+						startIcon={<FormatListBulletedRoundedIcon />}
+					>
+						To-Do
+					</Button>
+				</div>
 
-					<div>
-						<Button
-							size='medium'
-							variant='contained'
-							color='secondary'
-							href='notes'
-							startIcon={<NotesRoundedIcon />}
-						>
-							Notes
-						</Button>
-					</div>
+				<div>
+					<Button
+						size='medium'
+						variant='contained'
+						color='secondary'
+						href='notes'
+						startIcon={<NotesRoundedIcon />}
+					>
+						Notes
+					</Button>
 				</div>
 			</div>
 		</div>
